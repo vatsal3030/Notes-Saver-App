5c2a693a57333553ea4c8b14dada7a949f2b2355
Vatsal Vadgama <vatsalvadgama04@gmail.com>
2025-03-18 00:27:51 +0530

    ViewPaste.jsx Update 1.0

--- src/components/ViewPaste.jsx
+++ src/components/ViewPaste.jsx
@@ -6,31 +6,37 @@ const ViewPaste = () => {
   const { id } = useParams();
   const allPaste = useSelector((state) => state.paste.pastes);
   const paste = allPaste.filter((p) => p._id === id)[0];
-  console.log("final paste : ", paste);
+
   return (
-    <div>
-      <div className='flex felx-row gap-2 mt-4 place-content-between'>
-        <input disabled type='text' placeholder='Enter Title'
-          value={paste.title} onChange={(e) => { setTitle(e.target.value) }}
-          className='p-2 rounded-2xl bg-gray-700 w-[65%] pl-4' />
-        {/* <button className='p-2 rounded-2xl bg-gray-700' onClick={createPaste}>
-          {
-            pasteId ? "Update" : "Create My Paste"
-          }
-        </button> */}
-      </div >
-      <div className='mt-8'>
-        <textarea
+    <div className='overflow-x-hidden w-[800px]'>
+      <div className='flex flex-row gap-2 mt-4 place-content-between'>
+        <input
           disabled
-          value={paste.content}
-          className="mt-4 p-4 min-w-[500px] "
-          placeholder="Write your notes here..."
-          onChange={(e) => setValue(e.target.value)}
-          rows={20}
-        ></textarea>
+          type='text'
+          placeholder='Enter Title'
+          value={paste.title}
+          className="w-full max-w-[600px] p-3 bg-gray-800 text-white border border-gray-400 rounded focus:outline-none placeholder-gray-400"
+        />
+      </div>
+
+      <div className='w-full'>
+        <div className='w-full h-6 border border-gray-500 p-4 mt-4 flex flex-row justify-start items-center space-x-6 bg-gray-900'>
+          <div className='w-4 h-4 bg-red-600 rounded-[50%]'></div>
+          <div className='w-4 h-4 bg-blue-600 rounded-[50%]'></div>
+          <div className='w-4 h-4 bg-green-600 rounded-[50%]'></div>
+        </div>
+        <div className='w-full mt-0'>
+          <textarea
+            disabled
+            value={paste.content}
+            className="w-full p-4 bg-gray-900 text-white border border-gray-500 resize-none focus:outline-none placeholder-gray-400"
+            placeholder="Write your notes here..."
+            rows={20}
+          ></textarea>
+        </div>
       </div>
     </div>
   )
 }
 
-export default ViewPaste
+export default ViewPaste;
